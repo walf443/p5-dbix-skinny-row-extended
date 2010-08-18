7b7fa17c1d45c4f544dda8862b9b1dce22efb6a3
Keiji, Yoshimi <walf443@gmail.com>
2010-08-19 00:54:04 +0900

    initial commit

---
+++ lib/DBIx/Skinny/Row/Extended.pm
@@ -0,0 +1,33 @@
+package DBIx::Skinny::Row::Extended;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::Row::Extended -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::Row::Extended;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::Row::Extended is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
